c4d20b66feae384b42bdff5989bed8b71ab23ca0
smd67 <dimigstephenm@gmail.com>
2025-02-13 10:15:56 -0500

    Fix

--- server.py
+++ server.py
@@ -7,7 +7,9 @@
 from flask import Flask, render_template, request
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-#Initiate the flask app : TODO
+# pylint: disable=W0718
+
+#Initiate the flask app
 app = Flask(__name__)
 
 @app.route("/sentimentAnalyzer")
@@ -17,13 +19,13 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
     text = request.args.get('textToAnalyze')
     try:
         sentiment_dict = sentiment_analyzer(text)
-        return f"The given text has been identified as {sentiment_dict['label']} with a score of {sentiment_dict['score']}."
-    except Exception as e:
-        return f"Invalid input! Try again."
+        return (f"The given text has been identified as {sentiment_dict['label']} "
+                + "with a score of {sentiment_dict['score']}.")
+    except Exception:
+        return "Invalid input! Try again."
 
 
 @app.route("/")
@@ -31,12 +33,9 @@ def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    ''' 
-    This functions executes the flask app and deploys it on localhost:5000
-    '''
-    #TODO
+    # This functions executes the flask app and deploys it on localhost:5000
     app.run(port=5000, debug=True)
--- tests/test_sentiment_analyzer.py
+++ tests/test_sentiment_analyzer.py
@@ -2,12 +2,14 @@
 Tests for the sentiment_analyzer module.
 """
 
-import json
+import json as js
 from unittest.mock import MagicMock, patch
 
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-RESPONSE_JSON = json.loads(
+# pylint: disable=W0613, W0718
+
+RESPONSE_JSON = js.loads(
     '{"documentSentiment":{"score":0.996586, "label":"SENT_POSITIVE", "mixed":false, '
     + '"sentimentMentions":[{"span":{"begin":0, "end":27, "text":"I love this new technology."},'
     + ' "sentimentprob":{"positive":0.9934198, "neutral":0.0030947884, '
@@ -18,7 +20,7 @@ RESPONSE_JSON = json.loads(
 )
 
 
-RESPONSE_NEGATIVE_JSON = json.loads(
+RESPONSE_NEGATIVE_JSON = js.loads(
     '{"documentSentiment":{"score":0.50, "label":"SENT_NEGATIVE", "mixed":false, '
     + '"sentimentMentions":[{"span":{"begin":0, "end":27, "text":"I hate this new technology."},'
     + ' "sentimentprob":{"positive":0.9934198, "neutral":0.0030947884, '
@@ -29,7 +31,7 @@ RESPONSE_NEGATIVE_JSON = json.loads(
 )
 
 
-RESPONSE_NEUTRAL_JSON = json.loads(
+RESPONSE_NEUTRAL_JSON = js.loads(
     '{"documentSentiment":{"score":0.75, "label":"SENT_NEUTRAL", "mixed":false, '
     + '"sentimentMentions":[{"span":{"begin":0, "end":27, "text":"I meh this new technology."},'
     + ' "sentimentprob":{"positive":0.9934198, "neutral":0.0030947884, '
@@ -39,64 +41,99 @@ RESPONSE_NEUTRAL_JSON = json.loads(
     + '"producerId":{"name":"Aggregated Sentiment Workflow", "version":"0.0.1"}}'
 )
 
+
 @patch("requests.post")
 def test_sentiment_analyzer(mock_post):
+    """
+    Test sentiment_analyzer function.
+    """
+
     def mock_post_func(url, json, headers, timeout=10):
+        """
+        Mock post function.
+        """
         retval = MagicMock()
         retval.json.return_value = RESPONSE_JSON
         retval.text = RESPONSE_JSON
         retval.status_code = 200
         return retval
+
     mock_post.side_effect = mock_post_func
     sentiment_dict = sentiment_analyzer("I love this new technology.")
-    assert "label" in sentiment_dict.keys()
-    assert "score" in sentiment_dict.keys()
+    assert "label" in sentiment_dict
+    assert "score" in sentiment_dict
     assert sentiment_dict["label"] == "SENT_POSITIVE"
     assert sentiment_dict["score"] == 0.996586
 
+
 @patch("requests.post")
 def test_sentiment_analyzer_neutral(mock_post):
+    """
+    Test sentiment_analyzer function.
+    """
+
     def mock_post_func(url, json, headers, timeout=10):
+        """
+        Mock post function.
+        """
         retval = MagicMock()
         retval.json.return_value = RESPONSE_NEUTRAL_JSON
         retval.text = RESPONSE_NEUTRAL_JSON
         retval.status_code = 200
         return retval
+
     mock_post.side_effect = mock_post_func
     sentiment_dict = sentiment_analyzer("I meh this new technology.")
-    assert "label" in sentiment_dict.keys()
-    assert "score" in sentiment_dict.keys()
+    assert "label" in sentiment_dict
+    assert "score" in sentiment_dict
     assert sentiment_dict["label"] == "SENT_NEUTRAL"
     assert sentiment_dict["score"] == 0.75
 
+
 @patch("requests.post")
 def test_sentiment_analyzer_negative(mock_post):
+    """
+    Test sentiment_analyzer function.
+    """
+
     def mock_post_func(url, json, headers, timeout=10):
+        """
+        Mock post function.
+        """
         retval = MagicMock()
         retval.json.return_value = RESPONSE_NEGATIVE_JSON
         retval.text = RESPONSE_NEGATIVE_JSON
         retval.status_code = 200
         return retval
+
     mock_post.side_effect = mock_post_func
     sentiment_dict = sentiment_analyzer("I hate this new technology.")
-    assert "label" in sentiment_dict.keys()
-    assert "score" in sentiment_dict.keys()
+    assert "label" in sentiment_dict
+    assert "score" in sentiment_dict
     assert sentiment_dict["label"] == "SENT_NEGATIVE"
     assert sentiment_dict["score"] == 0.50
 
+
 @patch("requests.post")
 def test_sentiment_analyzer_bad_status_code(mock_post):
+    """
+    Test sentiment_analyzer function.
+    """
+
     def mock_post_func(url, json, headers, timeout=10):
+        """
+        Mock post function.
+        """
         retval = MagicMock()
         retval.json.return_value = RESPONSE_JSON
         retval.text = RESPONSE_JSON
         retval.status_code = 500
         retval.raise_for_status.side_effect = Exception("Bad status code")
         return retval
+
     mock_post.side_effect = mock_post_func
     try:
-        sentiment_dict = sentiment_analyzer("I love this new technology.")
+        _ = sentiment_analyzer("I love this new technology.")
         assert False
     except Exception as e:
         assert str(e) == "Bad status code"
-   
